cbaade5dcfaaf1af6449a38c05538eeb808ac2c0
msuban14 <msuban14@raf.rs>
2021-08-06 10:08:15 +0200

    added answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_075702) do
+ActiveRecord::Schema.define(version: 2021_08_06_080159) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.boolean "isCorrect"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -72,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_075702) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "lectures", "courses"
   add_foreign_key "questions", "lectures"
   add_foreign_key "questions", "question_difficulties"
